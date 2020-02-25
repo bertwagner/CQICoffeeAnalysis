6bc9055db4eb9cf7da142feff6566fef5089581b
Bert Wagner <github@bertwagner.com>
2020-02-25 05:51:15 -0500

    creates file for executing model

--- ExecuteModel.py
+++ ExecuteModel.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import numpy as np
 import joblib
-coffee_model_loaded = joblib.load("coffee_model.pkl")
 
-#data = pd.DataFrame(index=np.arange(1), columns=np.arange(42))
+def null_altitude_outliers(X):
+    X[X < 200] = None
+    X[X > 5000] = None
+    return X
 
-# data["Country.of.Origin"] = "Kenya"
-# data["Variety"] = "Bourbon"
-# data["Processing.Method"] = "Natural / Dry"
-# data["altitude_mean_meters"] = 2000
+model = joblib.load("coffee_model.pkl") 
 
-# result = full_pipeline_with_predictor.predict(data)
+data = pd.DataFrame(index=np.arange(0, 1),columns = ['Species', 'Owner', 'Country.of.Origin', 'Farm.Name', 'Lot.Number',
+       'Mill', 'ICO.Number', 'Company', 'Altitude', 'Region', 'Producer',
+       'Number.of.Bags', 'Bag.Weight', 'In.Country.Partner', 'Harvest.Year',
+       'Grading.Date', 'Owner.1', 'Variety', 'Processing.Method', 'Aroma',
+       'Flavor', 'Aftertaste', 'Acidity', 'Body', 'Balance', 'Uniformity',
+       'Clean.Cup', 'Sweetness', 'Cupper.Points', 'Moisture',
+       'Category.One.Defects', 'Quakers', 'Color', 'Category.Two.Defects',
+       'Expiration', 'Certification.Body', 'Certification.Address',
+       'Certification.Contact', 'unit_of_measurement', 'altitude_low_meters',
+       'altitude_high_meters', 'altitude_mean_meters'])
+
+data["Country.of.Origin"] = "Kenya"
+data["Variety"] = "Bourbon"
+data["Processing.Method"] = "Natural / Dry"
+data["altitude_mean_meters"] = 2000
+
+result = model.predict(data)
+print(result)
